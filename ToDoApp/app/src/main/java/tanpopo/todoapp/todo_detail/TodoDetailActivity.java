27b37e0aae1dcd38fe6da9891fb9946f645110c7
tanpopo <b2094@oic.jp>
2015-12-09 12:56:17 +0900

    setListenerなど追加

--- ToDoApp/app/src/main/java/tanpopo/todoapp/todo_detail/TodoDetailActivity.java
+++ ToDoApp/app/src/main/java/tanpopo/todoapp/todo_detail/TodoDetailActivity.java
@@ -2,17 +2,38 @@ package tanpopo.todoapp.todo_detail;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import tanpopo.todoapp.R;
 
 public class TodoDetailActivity extends ActionBarActivity {
+  EditText todoTitle;
+  Spinner categorySpinner;
+  CheckBox finishCheck;
+  EditText todoMemo;
+  ArrayAdapter adapter;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_todo_detail);
+
+    Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
+    setSupportActionBar(toolbar);
+    getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+    setListener();
+    setSpinner();
+    setValue();
   }
 
   @Override
@@ -35,5 +56,24 @@ public class TodoDetailActivity extends ActionBarActivity {
     }
 
     return super.onOptionsItemSelected(item);
+  }
+
+  private void setListener() {
+    todoTitle = (EditText)findViewById(R.id.todo_title);
+    categorySpinner = (Spinner)findViewById(R.id.categoies_spinner);
+    adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item);
+    finishCheck = (CheckBox)findViewById(R.id.finish_flag);
+    todoMemo = (EditText)findViewById(R.id.edit_memo);
+  }
+  private void setSpinner() {
+    List<String> categories = new ArrayList<>();
+    ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, categories);
+    categorySpinner.setAdapter(adapter);
+  }
+  private void setValue() {
+    todoTitle.setText("");
+    categorySpinner.setSelection(0);
+    finishCheck.setChecked(false);
+    todoMemo.setText("");
   }
 }
